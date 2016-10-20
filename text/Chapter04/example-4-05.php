1c60fa9d8cf454e22ccffdfeeee458ef4666a0b7
Yoshino Ryuki <e16006@std.it-college.ac.jp>
2016-10-20 20:08:20 +0900

    数値配列の作成

--- text/Chapter04/example-4-05.php
+++ text/Chapter04/example-4-05.php
@@ -1,8 +1,11 @@
+<?php
 // Create $lunch array with two elements
 // This sets $lunch[0]
 $lunch[] = 'Dried Mushrooms in Brown Sauce';
 // This sets $lunch[1]
 $lunch[] = 'Pineapple and Yu Fungus';
+
+var_dump($lunch);
 
 // Create $dinner with three elements
 $dinner = array('Sweet Corn and Asparagus', 'Lemon Chicken',
@@ -10,3 +13,5 @@ $dinner = array('Sweet Corn and Asparagus', 'Lemon Chicken',
 // Add an element to the end of $dinner
 // This sets $dinner[3]
 $dinner[] = 'Flank Skin with Spiced Flavor';
+
+var_dump($dinner);
