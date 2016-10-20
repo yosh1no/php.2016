dd3f5933f5affb46c984be99c616152c1b0e655a
Yoshino Ryuki <e16006@std.it-college.ac.jp>
2016-10-20 20:03:40 +0900

    配列の作成

--- text/Chapter04/example-4-01.php
+++ text/Chapter04/example-4-01.php
@@ -1,14 +1,21 @@
+<?php
 // An array called $vegetables with string keys
 $vegetables['corn'] = 'yellow';
 $vegetables['beet'] = 'red';
 $vegetables['carrot'] = 'orange';
+
+var_dump($vegetables);
 
 // An array called $dinner with numeric keys
 $dinner[0] = 'Sweet Corn and Asparagus';
 $dinner[1] = 'Lemon Chicken';
 $dinner[2] = 'Braised Bamboo Fungus';
 
+var_dump($dinner);
+
 // An array called $computers with numeric and string keys
 $computers['trs-80'] = 'Radio Shack';
 $computers[2600] = 'Atari';
 $computers['Adam'] = 'Coleco';
+
+var_dump($computers);
--- text/Chapter04/example-4-02.php
+++ text/Chapter04/example-4-02.php
@@ -1,11 +1,19 @@
+<?php
+
 $vegetables = array('corn' => 'yellow',
                     'beet' => 'red',
                     'carrot' => 'orange');
+
+var_dump($vegetables);
 
 $dinner = array(0 => 'Sweet Corn and Asparagus',
                 1 => 'Lemon Chicken',
                 2 => 'Braised Bamboo Fungus');
 
+var_dump($dinner);
+
 $computers = array('trs-80' => 'Radio Shack',
                    2600 => 'Atari',
                    'Adam' => 'Coleco');
+
+var_dump($computers);
